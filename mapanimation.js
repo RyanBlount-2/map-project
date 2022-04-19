5f5b510d156e08238ecf3758d0ad7e53c403a550
Ryan Blount <89652477+RyanBlount-2@users.noreply.github.com>
2022-04-19 11:41:35 -0400

    Update mapanimation.js

--- mapanimation.js
+++ mapanimation.js
@@ -1,67 +1,46 @@
-// Get access token at https://account.mapbox.com.
-mapboxgl.accessToken = 'pk.eyJ1IjoicnlhbmJsb3VudC0yIiwiYSI6ImNsMW1uZWx1YjAydGIzY205OXIxbDJ4bGYifQ.P-utZatLu35Br-uYeBQVVw';
+// Get access token at https://account.mapbox.com
+mapboxgl.accessToken = // 'Your Access Token Goes Here'
 
-// Define GeoJSON data.
-const geojson = {
-   type: 'FeatureCollection',
-   features: [
-      {
-         type: 'Feature',
-         geometry: {type: 'Point', coordinates: [-71.091542, 42.358862]},
-         properties: {title: 'Bus 1', description: 'Bus Route Between MIT & Harvard'}
-      }
-   ]
-};
-
-// Access the Mapbox API.
+// Accesses the Mapbox API
 let map = new mapboxgl.Map({
    container: 'map',
    style: 'mapbox://styles/mapbox/streets-v11',
-   center: [-71.091542, 42.358862],
-   zoom: 12
+   center: [-71.104081, 42.365554],
+   zoom: 13
 });
 
-// Add markers to the map (just one).
-for (const feature of geojson.features) {
-   // Create an HTML element for each feature (just one)
-   const bus = document.createElement('div');
-   bus.className = 'marker';
+// Create a DOM element for each marker.
+const busDiv = document.createElement('div');
+busDiv.className = 'marker';
 
-   // Make a marker for each feature and add it to the map (just one).
-   new mapboxgl.Marker(bus)
-      .setLngLat(feature.geometry.coordinates)
-      .setPopup(
-         // Add popups (just one)
-         new mapboxgl.Popup({offset: 25})
-            .setHTML(
-               '<h3>' + feature.properties.title + '</h3>' +
-               '<p>' + feature.properties.description + '</p>'
-            )
-      )
-      .addTo(map);
-}
+// Create a popup.
+let popup = new mapboxgl.Popup({ offset: 25 }).setText('Bus Current Location');
 
+// Adds a marker to the map
+let marker = new mapboxgl.Marker(busDiv)
+   .setLngLat([-71.092761, 42.357575])
+   .setPopup(popup)
+   .addTo(map);
+let counter = 0;
+
+// Gets the bus locations.
 async function getBusLocations() {
    const url = 'https://api-v3.mbta.com/vehicles?filter[route]=1&include=trip';
    const response = await fetch(url);
    const json = await response.json();
-   let coordinates = { 'lng': json.data[0].attributes.longitude, 'lat': json.data[0].attributes.latitude };
-   console.log(coordinates);
-   return coordinates;
+   let lat = json.data[0].attributes.latitude;
+   let lng = json.data[0].attributes.longitude;
+   let coordinates = { 'lng': lng, 'lat': lat };
+   marker.setLngLat(coordinates);
+   console.log(new Date());
+   console.log('Bus Location: ' + JSON.stringify(coordinates));
+   return json.data;
 }
 
+// Runs the program to update the map. *Note: setTimeout should not be timed too fast or the servers could block you.
 async function run() {
    let locations = await getBusLocations();
-   console.log(new Date());
-   console.log(locations);
-   marker.setLngLat(locations);
+   setTimeout(run, 30000);
 }
 
-async function move() {
-   setTimeout(() => {
-      run();
-      move();
-   }, 20000);
-}
-
-move();
+run();
